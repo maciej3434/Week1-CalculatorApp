ce6e99c2a5f9d88f27389772a1006ebcde56c1b4
Maciej Chroszcz <Chroszcz-M@ulster.ac.uk>
2025-09-29 18:57:21 +0100

    Implemented program code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ask user for first number.
+Console.WriteLine("Type in the first number followed by the Ender key");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+// Ask user for Second number.
+Console.WriteLine("Type in the Second number followed by the Ender key");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+// Perform Calulation
+int result = firstnumber + secondnumber;
+Console.WriteLine("The addition of both numbers is {0}", result);
+Console.ReadKey();
