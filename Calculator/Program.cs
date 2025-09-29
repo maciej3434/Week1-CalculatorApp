5a398230c8c41758a65ef5ed0b153355996beb9d
Maciej Chroszcz <Chroszcz-M@ulster.ac.uk>
2025-09-29 19:16:31 +0100

    Updated Program code with if-else statements.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,12 +1,45 @@
-﻿// Ask user for first number.
-Console.WriteLine("Type in the first number followed by the Ender key");
+﻿// Declared the variables and initialised them to 0.
+int FirstNumber, SecondNumber, Result, choice = 0;
+
+// Ask user for first number.
+Console.WriteLine("Type in the first number followed by the Enter key");
 int firstnumber = Convert.ToInt32(Console.ReadLine());
 
 // Ask user for Second number.
-Console.WriteLine("Type in the Second number followed by the Ender key");
+Console.WriteLine("Type in the Second number followed by the Enter key");
 int secondnumber = Convert.ToInt32(Console.ReadLine());
 
-// Perform Calulation
-int result = firstnumber + secondnumber;
-Console.WriteLine("The addition of both numbers is {0}", result);
-Console.ReadKey();
+
+// Asks user to choose an option.
+Console.WriteLine("Choose and option from the following list:");
+Console.WriteLine("1 - Add");
+Console.WriteLine("2 - Subtract");
+Console.WriteLine("3 - Divide");
+Console.WriteLine("4 - Multiply");
+
+choice = Convert.ToInt32(Console.ReadLine());
+
+if (choice == 1) 
+{
+    Result = FirstNumber + SecondNumber;
+    Console.WriteLine($"Adding {FirstNumber} and {SecondNumber} equals {Result}");
+}
+else if (choice == 2)
+{
+    Result = FirstNumber - SecondNumber;
+    Console.WriteLine($"Subtracting {FirstNumber} and {SecondNumber} equals {Result}");
+}
+else if (choice == 3)
+{
+    Result = FirstNumber / SecondNumber;
+    Console.WriteLine($"Dividing {FirstNumber} and {SecondNumber} equals {Result}");
+}
+else if (choice == 4)
+{
+    Result = FirstNumber * SecondNumber;
+    Console.WriteLine($"Multiplying {FirstNumber} and {SecondNumber} equals {Result}");
+}
+else
+{
+    Console.WriteLine("You have not typed a valid option between 1-4");
+}
